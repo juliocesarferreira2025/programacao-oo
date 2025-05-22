816ea6d0a643cf0c1527ce40f56d7624884e2bb2
juliocesarferreira2025 <julio.ferreira@utp.edu.br>
2025-05-21 22:20:03 -0300

    ed03.02
    comit

---
+++ ed03.02/src/ProgramaImpressao.java
@@ -0,0 +1,13 @@
+public class ProgramaImpressao {
+    public static void main(String[] args) {
+        DocumentoTexto doc = new DocumentoTexto("Documento de Apresentação", "Este é o conteúdo do documento.");
+        ImagemDigital img = new ImagemDigital("paisagem.jpg", "1920x1080");
+        GraficoEstatistico grafico = new GraficoEstatistico("Distribuição de Vendas", "barra");
+        RelatorioPDF relatorio = new RelatorioPDF("João Silva", 12);
+
+        doc.imprimir();
+        img.imprimir();
+        grafico.imprimir();
+        relatorio.imprimir();
+    }
+}
