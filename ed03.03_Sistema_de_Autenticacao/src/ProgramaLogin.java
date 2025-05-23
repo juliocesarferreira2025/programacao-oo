a560f58cda8009c1de417d676d284336acfc963d
juliocesarferreira2025 <julio.ferreira@utp.edu.br>
2025-05-23 20:21:39 -0300

    Update ProgramaLogin.java

--- ed03.03_Sistema_de_Autenticacao/src/ProgramaLogin.java
+++ ed03.03_Sistema_de_Autenticacao/src/ProgramaLogin.java
@@ -2,7 +2,7 @@ public class ProgramaLogin {
     public static void main(String[] args) {
         Usuario admin = new Administrador("rootMaster", "masterSecure!9");
         Usuario visitante = new Visitante("guest", "qualquer");
-        Usuario cliente = new Cliente("Aluizio", "v!8T#7kLp");
+        Usuario cliente = new Cliente("JULIO", "JCFS64970");
 
         // ADMINISTRADOR
         admin.exibirLogin();
@@ -27,7 +27,7 @@ public class ProgramaLogin {
         // CLIENTE
         cliente.exibirLogin();
         Autenticavel a2 = (Autenticavel) cliente;
-        System.out.println("Autenticação com 'v!8T#7kLp': " + a2.autenticar("v!8T#7kLp"));
+        System.out.println("Autenticação com 'JCFS64970': " + a2.autenticar("JCFS64970"));
         System.out.println("Autenticação com '12345': " + a2.autenticar("12345"));
     }
 }
